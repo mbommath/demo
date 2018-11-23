8236fde4389858c25f9325ea900c8674b1b053ab
bannapoo <bannapoo@DIN73002971.corp.capgemini.com>
2018-11-23 15:39:44 +0530

    hello stupid

--- demo1/src/main/java/org/capg/demo1/MainClass.java
+++ demo1/src/main/java/org/capg/demo1/MainClass.java
@@ -3,7 +3,7 @@ package org.capg.demo1;
 public class MainClass {
 	
 	public static void main(String[] args) {
-		System.out.println("hey man leave me alone!");
+		System.out.println("hey man leave me alone You stupid !");
 	}
 
 }
